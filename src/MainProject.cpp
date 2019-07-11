85ce0442a8bcaedd0f11773f4338032a2a29eb8c
Alireza Ahmadi <alireza.elecat@gmail.com>
2019-07-11 23:03:05 +0200

    Update MainProject.cpp

--- src/MainProject.cpp
+++ src/MainProject.cpp
@@ -1,9 +1,10 @@
 /*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%    A Host implementation of Bag of Visual Words        %
-% by: Alireza Ahmadi                                     %
-% University of Bonn- MSc Robotics & Geodetic Engineering%
-% Alireza.Ahmadi@uni-bonn.de                             %
-% alirezaahmadi.xyz                                      %
+%    A Host implementation of Bag of Visual Words        
+% by: Alireza Ahmadi and Robin Steffens  
+% Alireza.Ahmadi@uni-bonn.de
+% Robin.Steffens@gmx.net                                   
+% University of Bonn- MSc Robotics & Geodetic Engineering                  
+% alirezaahmadi.xyz                                      
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
 #include <include/BinLib.h>
 #include <include/Kmeans_Flann.h>
